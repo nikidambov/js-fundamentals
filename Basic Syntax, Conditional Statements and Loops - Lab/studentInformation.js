8cbe706e425a1c70f50c538f714631ab4e0bda01
niki <nikiwone@gmail.com>
2024-01-25 21:27:58 +0200

    solve studentInformation

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/studentInformation.js
@@ -0,0 +1,5 @@
+function studentInformation(name, age, averageGrade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${averageGrade.toFixed(2)}`);
+}
+
+studentInformation('John', 15, 5.54678);
