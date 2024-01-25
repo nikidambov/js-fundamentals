d4dedf9f6f3c2ca3fb55451c289fac68d25cb22d
niki <nikiwone@gmail.com>
2024-01-25 21:31:56 +0200

    solve excellentGrade

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/excellentGrade.js
@@ -0,0 +1,9 @@
+function excellentGrade(number) {
+    if (number >= 5.50) {
+        console.log("Excellent");
+    } else {
+        console.log("Not excellent");
+    }
+}
+
+excellentGrade(4.35);
