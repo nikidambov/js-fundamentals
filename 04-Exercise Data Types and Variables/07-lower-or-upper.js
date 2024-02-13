eeb533bff2640c0b26e3ae764e4d417b74b4f081
niki <nikiwone@gmail.com>
2024-02-13 19:25:30 +0200

    solved 07-lower-or-upper

---
+++ 04-Exercise Data Types and Variables/07-lower-or-upper.js
@@ -0,0 +1,12 @@
+function lowerOrUpper(char) {
+
+    char === char.toUpperCase() ? console.log('upper-case') : console.log('lower-case');
+
+    //if (char === char.toUpperCase()) {
+    //    console.log("upper-case");
+    //} else if (char === char.toLowerCase()) {
+    //    console.log("lower-case");
+    //}
+}
+
+lowerOrUpper("L");
