1c68aabdc2666d32293991173ba0993f791e9517
niki <nikiwone@gmail.com>
2024-02-17 19:59:00 +0200

    solved 04-array-rotation

---
+++ 06-Exercise Arrays/04-array-rotation.js
@@ -0,0 +1,10 @@
+function arrayRotation(arr, rotations) {
+    for (let i = 0; i < rotations; i++) {
+        let firstElement = arr.shift();
+        arr.push(firstElement);
+    }
+
+    console.log(arr.join(" "));
+}
+
+arrayRotation([51, 47, 32, 61, 21], 2);
